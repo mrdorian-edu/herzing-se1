e8f45ddd0671466f30121dd4dfa46fac07b8013c
mrdorian-edu <113300264+mrdorian-edu@users.noreply.github.com>
2022-09-11 15:27:33 -0500

    added score tracker & screenshot

--- unit1/RockPapersScissors/RockPapersScissors.py
+++ unit1/RockPapersScissors/RockPapersScissors.py
@@ -6,10 +6,19 @@
 
 import random
 
+#game
 def main():
-  print ("Rock, Papers, Scissors")
-  print ("----------------------")
 
+  #title
+  print ("Rock, Papers, Scissors")
+  print ("-------------------------")
+
+  #define variables
+  playerScore = 0
+  computerScore = 0
+  tieScore = 0
+
+  #loop until user ends the game
   while True:
     user_input = input("\nEnter a choice (rock, paper, scissors): ")
     possible_choices = ["rock", "paper", "scissors"]
@@ -19,23 +28,36 @@ def main():
 
     if user_input == result:
         print(f"Both players selected {user_input}. It's a tie!")
+        tieScore += 1
     elif user_input == "rock":
         if result == "scissors":
             print("Rock smashes scissors! You win!")
+            playerScore += 1
         else:
             print("Paper covers rock! You lose.")
+            computerScore += 1
     elif user_input == "paper":
         if result == "rock":
             print("Paper covers rock! You win!")
+            playerScore += 1
         else:
             print("Scissors cuts paper! You lose.")
+            computerScore += 1
     elif user_input == "scissors":
         if result == "paper":
             print("Scissors cuts paper! You win!")
+            playerScore += 1
         else:
             print("Rock smashes scissors! You lose.")
+            computerScore += 1
 
-    again = input("\nPlay again? (y/n): ")
+    #current score
+    print ("\nCurrent Score")
+    print ("-------------------------")
+    print ("You: " +str(playerScore)+ ", Computer: " +str(computerScore)+ ", Tie: " +str(tieScore)+"\n")
+
+    again = input("> Play again? (y/n): ")
+
     if again.lower() != "y":
         print("\nFine then, I didn't want to play with you anyways!\n")
         break
