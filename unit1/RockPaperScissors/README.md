8b5d41847d0927b8c146c3966b51e50798211097
mrdorian-edu <113300264+mrdorian-edu@users.noreply.github.com>
2022-09-11 15:39:34 -0500

    rock, paper, scissors
    Version 0.1

---
+++ unit1/RockPaperScissors/README.md
@@ -0,0 +1,8 @@
+# Author: Dorian Brandusa
+# University: Herzing University
+# Class: IS341 - Softare Engineering I
+# Unit: 1
+
+# App: RockPaperScissors v1.0
+
+![alt text](https://github.com/mrdorian-edu/herzing-se1/blob/main/unit1/RockPaperScissors/screenshot.png?raw=true)
---
+++ unit1/RockPaperScissors/RockPaperScissors.py
@@ -0,0 +1,66 @@
+#HERZING UNIVERSITY
+#DBRANDUSA@HERZING.EDU
+#UNIT1
+
+#!/usr/bin/env python3
+
+import random
+
+#app
+def rockPaperScissors():
+
+  #title
+  print ("Rock, Paper, Scissors v0.1")
+  print ("--------------------------")
+
+  #define variables
+  playerScore = 0
+  computerScore = 0
+  tieScore = 0
+
+  #loop until user ends the game
+  while True:
+    user_input = input("\nEnter a choice (rock, paper, scissors): ")
+    possible_choices = ["rock", "paper", "scissors"]
+    result = random.choice(possible_choices)
+
+    print(f"\nYou chose {user_input}, computer chose {result}.\n")
+
+    if user_input == result:
+        print(f"Both players selected {user_input}. It's a tie!")
+        tieScore += 1
+    elif user_input == "rock":
+        if result == "scissors":
+            print("Rock smashes scissors! You win!")
+            playerScore += 1
+        else:
+            print("Paper covers rock! You lose.")
+            computerScore += 1
+    elif user_input == "paper":
+        if result == "rock":
+            print("Paper covers rock! You win!")
+            playerScore += 1
+        else:
+            print("Scissors cuts paper! You lose.")
+            computerScore += 1
+    elif user_input == "scissors":
+        if result == "paper":
+            print("Scissors cuts paper! You win!")
+            playerScore += 1
+        else:
+            print("Rock smashes scissors! You lose.")
+            computerScore += 1
+
+    #current score
+    print ("\nCurrent Score")
+    print ("--------------------------")
+    print ("You: " +str(playerScore)+ ", Computer: " +str(computerScore)+ ", Tie: " +str(tieScore)+"\n")
+
+    again = input("> Play again? (y/n): ")
+
+    if again.lower() != "y":
+        print("\nFine then, I didn't want to play with you anyways!\n")
+        break
+
+#initiate
+rockPaperScissors()
